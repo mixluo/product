6657dce51ed5a25e3a9bba8998276ffee1e7e38b
mixluo <luoqiang92@126.com>
2018-08-18 15:05:21 +0800

    rabbitmq使用

--- server/src/main/java/com/imooc/product/service/impl/ProductServiceImpl.java
+++ server/src/main/java/com/imooc/product/service/impl/ProductServiceImpl.java
@@ -8,11 +8,14 @@ import com.imooc.product.enums.ResultEnum;
 import com.imooc.product.exception.ProductException;
 import com.imooc.product.repository.ProductInfoRepository;
 import com.imooc.product.service.ProductService;
+import com.imooc.product.utils.JsonUtil;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,6 +24,8 @@ import java.util.stream.Collectors;
 public class ProductServiceImpl implements ProductService{
     @Autowired
     ProductInfoRepository productInfoRepository;
+    @Autowired
+    AmqpTemplate amqpTemplate;
     public List<ProductInfo> findUpAll() {
         return productInfoRepository.findByProductStatus(ProductStatusEnum.UP.getCode()) ;
     }
@@ -33,8 +38,26 @@ public class ProductServiceImpl implements ProductService{
         }).collect(Collectors.toList());
     }
 
-    @Transactional
     public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList){
+        List<ProductInfo> productInfoList = decreaseStockProcess(decreaseStockInputList);
+
+        //发送MQ消息
+        List<ProductInfoOutput> productInfoOutputList = productInfoList.stream().map(e ->{
+            ProductInfoOutput output = new ProductInfoOutput();
+            BeanUtils.copyProperties(e,output);
+            return output;
+        }).collect(Collectors.toList());
+        amqpTemplate.convertAndSend("productInfo", JsonUtil.toJson(productInfoOutputList));
+    }
+
+    /**
+     * 处理购物车传过来的信息
+     * @param decreaseStockInputList
+     * @return
+     */
+    @Transactional
+    public List<ProductInfo> decreaseStockProcess(List<DecreaseStockInput> decreaseStockInputList){
+        List<ProductInfo> productInfoList = new ArrayList<ProductInfo>();
         for(DecreaseStockInput decreaseStockInput : decreaseStockInputList){
             Optional<ProductInfo> productInfoOptional = productInfoRepository.findById(decreaseStockInput.getProductId());
             if(!productInfoOptional.isPresent()){
@@ -48,6 +71,8 @@ public class ProductServiceImpl implements ProductService{
             }
             productInfo.setProductStock(result);
             productInfoRepository.save(productInfo);
+            productInfoList.add(productInfo);
         }
+        return productInfoList;
     }
 }
