1e9eecfd078beaaeab63f3779ce609d0d3f0cabf
mixluo <luoqiang92@126.com>
2018-10-25 21:04:47 +0800

    在配置中心增加网关服务的配置文件

--- client/src/main/java/com/imooc/product/client/ProductClient.java
+++ client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -13,7 +13,7 @@ import java.util.List;
  * Created by 廖师兄
  * 2017-12-10 21:04
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product",fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -21,4 +21,18 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient{
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
